9239ae5892d2a0c0d424145ee03c484eacf406bd
Yeisonryhn <yeisonf15@gmail.com>
2018-11-03 11:55:26 -0430

    Terminado Windows y refactorizacion de código

--- Codigo/main.c
+++ Codigo/main.c
@@ -19,64 +19,57 @@ void menu(char* clave){
         printf("\n3.- Ver mensaje.");
         printf("\n4.- Eliminar mensaje.");
         printf("\n0.- Salir.");
-
         printf("\n\nIngresar la opcion: ");
         setbuf(stdin,NULL);
-        scanf("%i%*c", &r);
-        
+        scanf("%i%*c", &r);        
 
         switch(r){
 
             case 1:
-                //limpiar();
+            	
                 printf("\n");
                 printf("Ingrese mensaje: ");
                 setbuf(stdin,NULL);
                 scanf("%[^\n]", valor);
                 agregar_msg(clave, valor);
+                
             break;
 
             case 2:
-                //limpiar();
+            	
                 printf("\n");
-                #ifdef __unix__
-				modificar_msg(clave);
-				#elif defined(_WIN32) || defined(WIN32)
-				
-				#endif
+                modificar_msg(clave);
+												
             break;
 
             case 3:
-                //limpiar();
-                printf("\n");
+                
+				printf("\n");
                 consultar_msg(clave);  
-                esperar();     
-            break;
+            
+			break;
 
             case 4:
-                //limpiar();
-                #ifdef __unix__				
-                destruir_msg(clave);
-                #elif defined(_WIN32) || defined(WIN32)
                 
-				#endif
-                esperar();    
-            break;
+				destruir_msg(clave);
+            
+			break;
 
             case 0:
+            	
                 limpiar();
-                //printf("Saliendo del programa.\n");
-                //esperar();
-            break;
+            
+			break;
 
             default:
+            	
                 limpiar();
                 printf("Opcion incorrecta, vuelve a intentarlo.\n");
                 esperar();
+                
             break;
             
-        }
-        
+        }        
 
     }while(r != 0);
 
@@ -89,9 +82,13 @@ int main(int argc, char** argv) {
     printf("Ingresar el nombre de la sala, maximo 20 caracteres: ");
     fflush(stdin);
     scanf("%[^\n]", clave);
-    printf("%s", clave);    
-    printf("%d", atoi(clave));
+    //FALTA ARREGLAR QUE EN LINUX LA CLAVE DE LA MEMORIA COMPARTIDA SEA UNA CADENA 
+    //ES POR ESTO QUE AUN SE HACE LO SIGUIENTE
+    #ifdef __unix__
+    menu(atoi(clave));
+    #elif defined(_WIN32) || defined(WIN32)
     menu(clave);
+    #endif
 
 	return 0;
 }
--- Codigo/memcomp.h
+++ Codigo/memcomp.h
@@ -19,8 +19,6 @@
 
 void esperar(){
 	
-    
-
     #ifdef __unix__
         printf("\nPulsa enter para continuar...");
         while(getchar()!='\n');
@@ -32,163 +30,253 @@ void esperar(){
 
 void limpiar(){
 
-    
-
     #ifdef __unix__
         system("clear");
     #elif defined(_WIN32) || defined(WIN32)
         system("cls");
-
     #endif     
 
 }
 
-//---------------------------------METODO AGREGAR MENSAJE EN LINUX Y WINDOWS-----------------------------------------------------------------------
 #ifdef __unix__
+//-----------------------------------------------------------------------UNIX-----------------------------------------------------------------------
+//AGREGAR
 void agregar_msg(int clave, char valor[MAX_CHARS]){
 	
-	//else if(!OS_Windows){
-
-        char *p;
-        int varComp = shmget((key_t) clave, sizeof(int), IPC_CREAT|0666);
-
-        p = shmat(varComp, NULL, 0);
-        strcpy(p, valor);
-        shmdt(p);
+	char *p;
+    int varComp = shmget((key_t) clave, sizeof(int), IPC_CREAT|0666);
+    p = shmat(varComp, NULL, 0);
+    strcpy(p, valor);
+    shmdt(p);
+    printf("Mensaje agregado\n");
+    esperar();
 
 }
-
-#elif defined(_WIN32) || defined(WIN32)
-void agregar_msg(char* clave, char valor[MAX_CHARS]){
-	
-        LPCTSTR p;       
-		HANDLE varComp;
-		varComp = CreateFileMapping( INVALID_HANDLE_VALUE,NULL,PAGE_READWRITE,0,sizeof(int),clave);
-
-        if(varComp != NULL){
-
-            p = (LPTSTR) MapViewOfFile(varComp, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(int));
-            CopyMemory((PVOID) p, valor, sizeof(TCHAR) * MAX_CHARS);
-            UnmapViewOfFile(p);
-
-        }else{
-             _tprintf(TEXT("Could not create file mapping object (%d).\n"),GetLastError());
-        }
-}
-#endif
-
-//---------------------------------METODO CONSULTAR MENSAJE EN LINUX Y WINDOWS------------------------------------------------------------------
-#ifdef __unix__
+//CONSULTAR
 void consultar_msg(int clave){
 
-	
-   		char *p, valor[MAX_CHARS];
-        int varComp = shmget((key_t) clave, sizeof(int), IPC_CREAT|0666);
+	char *p, valor[MAX_CHARS];
+    int varComp = shmget((key_t) clave, sizeof(int), IPC_CREAT|0666);        
+    p = shmat(varComp,NULL,0);
+
+    if(strcmp(p, "") == 0){
+    	
+        printf("No existen mensajes para consultar...\n");
         
-        p = shmat(varComp,NULL,0);
-
-        if(strcmp(p, "") == 0){
-            printf("No existen mensajes para consultar...");
-        }else{
-            printf("%s", p);
-        }
-
-        shmdt(p);
-
-   
+    }else{
+    	
+        printf("%s", p);
         
-}
-#elif defined(_WIN32) || defined(WIN32)
-void consultar_msg(char clave[MAX_CHARS]){  
-		HANDLE varComp;
-		varComp = OpenFileMapping( FILE_MAP_ALL_ACCESS, FALSE, clave);
-		printf("funcion");
-		if(varComp != NULL){
-			
-			LPCTSTR p = (LPTSTR) MapViewOfFile(varComp, FILE_MAP_READ, 0, 0, sizeof(TCHAR) * MAX_CHARS);
-			printf("El valor actual es %s\n\n", p);
-			UnmapViewOfFile(p);
-		} }else{
-             _tprintf(TEXT("Could not access file mapping object (%d).\n"),GetLastError());
-        }
-}
-        
-#endif    
-
-//-------------------------------------LOS OTROS DOS METODOS QUE AUN NO SE HAN ARREGLADO ESTAN AQUI------------------------------------------------
-#ifdef __unix__
-void modificar_msg(int clave){
-
-    if(OS_Windows){
-
-
-    }else if(!OS_Windows){
-
-        char *p, valor[MAX_CHARS];
-        int varComp = shmget((key_t) clave, sizeof(int), IPC_CREAT|0666),posicion,i;
-        
-        p = shmat(varComp,NULL,0);
-
-        if(strcmp(p, "") == 0){
-            printf("No existen mensajes para modificar...");
-            esperar();
-
-        }else{
-
-            if(strstr(p, "(modificado)")){
-
-                //extrae la posicion de la subcadena
-                posicion = strlen( p ) - strlen( strstr( p,"(modificado)" ) );
-
-                //copia en una nueva variable hasta donde está la subcadena
-                for(i = 0; i < posicion; i++ ){
-                    valor[ i ] = p[ i ];    
-                }   
-                //agrega el cierre de linea
-                valor[ i ] = '\0';
-            }else{
-
-                strcpy(valor,p);
-            }               
-
-                printf("Mensaje a modificar: %s\n", valor);
-                printf("Modificar: ");
-                scanf("%[^\n]", valor);//lee cadenas de texto con varias palabras
-                strcat(valor, " (modificado).");
-                strcpy(p, valor);
-        }
-
-        shmdt(p);
-
     }
-
+	
+	esperar();
+    shmdt(p);  
+        
 }
+//MODIFICAR
+void modificar_msg(int clave){  
 
+    char *p, valor[MAX_CHARS];
+    int varComp = shmget((key_t) clave, sizeof(int), IPC_CREAT|0666),posicion,i;
+    p = shmat(varComp,NULL,0);
 
-
-
-
+    if(strcmp(p, "") == 0){
+    	
+        printf("No existen mensajes para modificar...\n");
+        
+    }else{
+    	
+        if(strstr(p, "(modificado)")){
+            //extrae la posicion de la subcadena
+            posicion = strlen( p ) - strlen( strstr( p,"(modificado)" ) );
+            //copia en una nueva variable hasta donde está la subcadena
+            for(i = 0; i < posicion; i++ ){
+            	
+                valor[ i ] = p[ i ];   
+				 
+            }   
+            //agrega el cierre de linea
+            valor[ i ] = '\0';
+            
+        }else{
+        	
+            strcpy(valor,p);
+            
+        }         
+		      
+            printf("Mensaje a modificar: %s\n", valor);
+            printf("Modificar: ");
+            scanf("%[^\n]", valor);//lee cadenas de texto con varias palabras
+            strcat(valor, " (modificado).");
+            strcpy(p, valor);
+            printf("El Mensaje ha sido modificado: %s\n\n", valor);
+            
+    }
+    
+	esperar();
+    shmdt(p);
+    
+}
+//ELIMINAR
 void destruir_msg(int clave){
 
-    if(OS_Windows){
-
-    }else if(!OS_Windows){
-
-        char *p;
-        int varComp = shmget((key_t) clave, sizeof(int), IPC_CREAT|0666);
-
-        p = shmat(varComp,NULL,0);
-
-        if(strcmp(p, "") == 0){
-            printf("\nNo existen mensajes para eliminar...");
-        }else{
-            strcpy(p, "");
-            printf("\nMensaje eliminado...");
-        }
-
-        shmdt(p);
-
+    char *p;
+    int varComp = shmget((key_t) clave, sizeof(int), IPC_CREAT|0666);
+    p = shmat(varComp,NULL,0);
+    
+    if(strcmp(p, "") == 0){
+    	
+	    printf("\nNo existen mensajes para eliminar...\n");
+	    
+    }else{
+    	
+        strcpy(p, "");
+        printf("\nMensaje eliminado...\n");
     }
+    
+    esperar();
+    shmdt(p);
+}
 
+#elif defined(_WIN32) || defined(WIN32)
+//-----------------------------------------------------------------WINDOWS--------------------------------------------------------------------------
+//AGREGAR
+void agregar_msg(char* clave, char valor[MAX_CHARS]){
+	
+    LPCTSTR p;       
+	HANDLE varComp;
+	varComp = CreateFileMapping( INVALID_HANDLE_VALUE,NULL,PAGE_READWRITE,0,sizeof(int),clave);
+
+    if(varComp != NULL){
+
+        p = (LPTSTR) MapViewOfFile(varComp, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(int));
+        CopyMemory((PVOID) p, valor, sizeof(TCHAR) * MAX_CHARS);
+        UnmapViewOfFile(p);
+		printf("Mensaje agregado\n");
+		
+    }else{
+    	
+        _tprintf(TEXT("Could not create file mapping object (%d).\n"),GetLastError());
+        
+    }
+    
+    esperar();
+    
+}
+//CONSULTAR
+void consultar_msg(char clave[MAX_CHARS]){  
+
+	HANDLE varComp;
+	varComp = OpenFileMapping( FILE_MAP_ALL_ACCESS, FALSE, clave);
+		
+	if(varComp != NULL){
+			
+		LPCTSTR p = (LPTSTR) MapViewOfFile(varComp, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(TCHAR) * MAX_CHARS);
+		
+		if(strcmp(p, "") == 0){
+			
+			printf("No existen mensajes para consultar...\n");
+			
+		}else{
+			
+			printf("El valor actual es %s\n\n", p);
+			UnmapViewOfFile(p);
+			
+		}
+			
+	}else{
+		
+        _tprintf(TEXT("No existen mensajes para consultar...\n"),GetLastError());
+        
+    }
+    
+    esperar();
+    
+}
+//MODIFICAR
+void modificar_msg(char clave[MAX_CHARS]){  
+
+	HANDLE varComp;		
+	varComp = OpenFileMapping( FILE_MAP_ALL_ACCESS, FALSE, clave);
+	int posicion,i;
+	char valor[MAX_CHARS];
+	
+	if(varComp != NULL){
+				
+		LPCTSTR p = (LPTSTR) MapViewOfFile(varComp, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(TCHAR) * MAX_CHARS);
+		
+		if(strcmp(p, "") == 0){
+			
+	        printf("No existen mensajes para modificar...\n");
+	    }else{
+	    	
+				
+			if(strstr(p, "(modificado)")){
+				
+				posicion = strlen( p ) - strlen( strstr( p,"(modificado)" ) );
+				
+				for(i = 0; i < posicion; i++ ){
+					
+	                valor[ i ] = p[ i ];    
+	            }
+	            
+	            valor[ i ] = '\0';
+	            
+			}else{
+				
+				strcpy(valor,p);
+				
+			}
+					
+			printf("Mensaje a modificar: %s\n\n", valor);
+			printf("Modificar: ");
+	        scanf("%[^\n]", valor);//lee cadenas de texto con varias palabras
+	        strcat(valor, " (modificado).");
+	        CopyMemory((PVOID) p, valor, sizeof(TCHAR) * MAX_CHARS);
+	        printf("El Mensaje ha sido modificado: %s\n\n", valor);
+	        
+	    }
+	    
+		UnmapViewOfFile(p);	
+		
+	}else{
+		
+        _tprintf(TEXT("No existen mensajes para modificar...\n"),GetLastError());
+        
+    }
+        
+    esperar();
+}
+//ELIMINAR
+void destruir_msg(char clave[MAX_CHARS]){ 
+ 
+	HANDLE varComp;		
+	varComp = OpenFileMapping( FILE_MAP_ALL_ACCESS, FALSE, clave);
+	char valor[MAX_CHARS];
+	
+	if(varComp != NULL){
+				
+		LPCTSTR p = (LPTSTR) MapViewOfFile(varComp, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(TCHAR) * MAX_CHARS);
+		
+		if(strcmp(p, "") == 0){
+			
+			printf("\nNo existen mensajes para eliminar...\n");	
+			            
+		}else{
+			
+			strcpy(valor,"");
+			
+		}
+		
+		CopyMemory((PVOID) p, valor, sizeof(TCHAR) * MAX_CHARS);
+		UnmapViewOfFile(p);	
+		esperar();
+		
+	}else{
+		
+        _tprintf(TEXT("No existen mensajes para eliminar.\n"),GetLastError());
+        esperar();
+        
+    }
 }
 #endif
